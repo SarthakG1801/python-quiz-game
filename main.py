3e3fdf20548e909286f11c77c1ea8cbe95af5964
Sarthak Gupta <sarthakgupta1971@gmail.com>
2025-08-01 12:56:10 +0530

    add questions and score

--- main.py
+++ main.py
@@ -13,3 +13,31 @@ if answer.lower()=="central processing unit":
     score+=1
 else:
     print("INCORRECT")
+
+
+answer=input("What does RAM stand for ? ")
+if answer.lower()=="random access memory":
+    print("CORRECT!!!")
+    score+=1
+else:
+    print("INCORRECT")
+
+
+answer=input("What does PSU stand for ? ")
+if answer.lower()=="power supply":
+    print("CORRECT!!!")
+    score+=1
+else:
+    print("INCORRECT")
+
+
+answer=input("What does GPU stand for ? ")
+if answer.lower()=="graphics processing unit":
+    print("CORRECT!!!")
+    score+=1
+else:
+    print("INCORRECT")
+
+
+print("you got " + str(score) + " questions correct. ")
+print("you got " + str((score/4)*100) + " %. ")
